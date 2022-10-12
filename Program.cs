2ae10150b1254d244db17d749ede769c221ed943
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-12 22:41:44 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day8LineComparisonProblem
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Problem");
+        }
+    }
+}
