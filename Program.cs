62ccad98a799355b9746900e47741d6f264b00f9
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-12 23:12:31 +0530

    comparing 2 lines using CompareTo Method

--- Program.cs
+++ Program.cs
@@ -12,35 +12,38 @@
             double Y = Math.Pow(y2 - y1, 2.0);
             length_Of_Line = Math.Sqrt(X + Y);
         }
-
     }
     public class Program
     {
         public static void Main(String[] args)
         {
-            //line 1 object creating and reading input from user
-            LineComparison lc = new LineComparison();
-            Console.WriteLine("enter x1,y1,x2,y2 values");
+            // creating object 1 and reading input from user
+            LineComparison lc1 = new LineComparison();
+            Console.WriteLine("enter line 1 values x1,y1,x2,y2 values\n");
             double a = Convert.ToDouble(Console.ReadLine());
             double b = Convert.ToDouble(Console.ReadLine());
             double c = Convert.ToDouble(Console.ReadLine());
             double d = Convert.ToDouble(Console.ReadLine());
-            lc.Calc(a, b, c, d);
-            Console.WriteLine("Line Length is = " + lc.length_Of_Line);
+            lc1.Calc(a, b, c, d);
+            Console.WriteLine("Line1 Length is = " + lc1.length_Of_Line);
 
-            //line 2 object creating and reading input from user
-            Console.WriteLine("Enter Sencond line values x1,y1,x2,y2");
+            //creating object 2 and reading input from user
+            LineComparison lc2 = new LineComparison();
+            Console.WriteLine("Enter Sencond line values x1,y1,x2,y2\n");
             double e = Convert.ToDouble(Console.ReadLine());
             double f = Convert.ToDouble(Console.ReadLine());
             double g = Convert.ToDouble(Console.ReadLine());
             double h = Convert.ToDouble(Console.ReadLine());
-            LineComparison lc2 = new LineComparison();
             lc2.Calc(e, f, g, h);
             Console.WriteLine("Line 2 Length is = " + lc2.length_Of_Line);
 
-            //compairing by both line values using Equals method
-            Console.WriteLine("Checking if the lines are equal, the answer is:");
-            Console.WriteLine((lc.length_Of_Line).Equals(lc2.length_Of_Line));
+            //comparing both line values to if its equal Returns True if not equal then returns False
+            Console.WriteLine("Checking lines are equal the answer is");
+            Console.WriteLine((lc1.length_Of_Line).Equals(lc2.length_Of_Line));
+
+            //comparing both line values to find line 1 is greater than line 2 Returns "1" if line 1 is greater then line, if line 1 is less than line 2 "-1"
+            Console.WriteLine("Compairing line 1 to line 2 \nif returned 1 :line 1 is greater than line 2\nif returned -1 :line 1 is less than line 2");
+            Console.WriteLine((lc1.length_Of_Line).CompareTo(lc2.length_Of_Line));
         }
 
     }
